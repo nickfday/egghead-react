f372957de820173b26068c38f038003f8f89785c
Nick Finley-Day <nickfday@gmail.com>
2017-03-16 19:29:28 +0000

    added lesson 5 code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
 class App extends React.Component {
+	// Gives 'this' the context within the component rather than parent class
+	constructor(){
+		super();
+		this.state = {
+			txt: 'this is the state txt',
+			cat: 0
+		}
+	}
+
+	//custom method
+	update ( e ) {
+		this.setState({txt: e.target.value})
+	}
+
 	render() {
-		let txt = this.props.txt
-		return <div>Hi {txt}</div>
+		// {} interpolates the state objects value
+		  return (
+		    <div>
+		    <input type="text"
+		      onChange={this.update.bind(this)}/>
+		      <h1>{this.state.txt} - {this.state.cat}</h1>
+		    </div>
+		)
 	}
 }
 
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt: "this is the default text"
-}
-
 export default App;
+
+/* Props
+A collection of values that are passed in as static values
+
+State
+A collection of values that are managed and updated by the components
+
+*/
 
