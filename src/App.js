cc2a0c2034ca82a6580dd107a528b9a5c3885e23
Nick Finley-Day <nickfday@gmail.com>
2017-03-16 23:49:31 +0000

    added lesson code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-/* 8 - Add Custom Validation propType
-
-Built in Validation limited
-e.g. React.PropTypes.string.isRequired
-
-Custom Function More Powerful
-
-
-
+/* 9 - Synthetic event system
+Normalises all events we want to use across browsers
 */
 
 
 import React from 'react';
 
 class App extends React.Component {
-	render() {
-		return <Title text="The"/>
+	constructor() {
+		super();
+		this.state = {currentEvent: '---'}
+		this.update = this.update.bind(this)
 	}
-}
+	update(e) {
+		this.setState({currentEvent: e.type})
+	}
+	render() {
+		return (
+			<div>
+				<textarea
+					onKeyPress={this.update}
+					onCopy={this.update}
+					onCut={this.update}
+					onPaste={this.update}
+					onFocus={this.update}
+					onBlur={this.update}
 
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-// Custom Function
-Title.propTypes = {
-	text(props, propName, component){
-		//isRequired
-		if(!(propName in props)) {
-			return new Error(`missing ${propName}`)
-		}
-		//length check
-		if(props[propName].length < 6) {
-			return new Error (`${propName} was too short`)
-		}
+					cols="30"
+					rows="10"	/>
+				<h1>{this.state.currentEvent}</h1>
+			</div>
+		)
 	}
 }
 
